b5225fd7bd202cff5e546ab8bd7f8c480fe23a24
Aleksandr <averianovai@gmail.com>
2023-02-09 17:13:22 +0300

    fill test

--- config/configHelpers.go
+++ config/configHelpers.go
@@ -1,18 +1,17 @@
 package config
 
 import (
+	"log"
 	"os"
 	"strconv"
 	"strings"
-
-	"sharesms-server/pkg/logging"
 )
 
 func mustEnvStr(key string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	} else {
-		logging.Default.Panicf("Environment variable %v must be set.", key)
+		log.Panicf("Environment variable %v must be set.", key)
 		return ""
 	}
 }
@@ -42,7 +41,7 @@ func optionalEnvBool(key string, fallback bool) bool {
 	if strValue, ok := os.LookupEnv(key); ok {
 		boolValue, err := strconv.ParseBool(strValue)
 		if err != nil {
-			logging.Default.Panicf("Failed to parse environment variable as bool %v with value %v.", key, strValue)
+			log.Panicf("Failed to parse environment variable as bool %v with value %v.", key, strValue)
 			return fallback
 		}
 		return boolValue
@@ -54,7 +53,7 @@ func optionalEnvInt(key string, fallback int) int {
 	if strValue, ok := os.LookupEnv(key); ok {
 		intValue, err := strconv.Atoi(strValue)
 		if err != nil {
-			logging.Default.Panicf("Failed to parse environment variable as int %v with value %v.", key, strValue)
+			log.Panicf("Failed to parse environment variable as int %v with value %v.", key, strValue)
 			return fallback
 		}
 		return intValue
@@ -66,7 +65,7 @@ func optionalEnvInt64(key string, fallback int64) int64 {
 	if value, ok := os.LookupEnv(key); ok {
 		int64Value, err := strconv.ParseInt(value, 10, 64) // str to int64
 		if err != nil {
-			logging.Default.Panicf("Failed to parse environment variable as int64 %v with value %v.", key, value)
+			log.Panicf("Failed to parse environment variable as int64 %v with value %v.", key, value)
 			return fallback
 		}
 		return int64Value
@@ -78,12 +77,12 @@ func mustEnvInt64(key string) int64 {
 	if value, ok := os.LookupEnv(key); ok {
 		int64Value, err := strconv.ParseInt(value, 10, 64) // str to int64
 		if err != nil {
-			logging.Default.Panicf("Failed to parse environment variable as int64 %v with value %v.", key, value)
+			log.Panicf("Failed to parse environment variable as int64 %v with value %v.", key, value)
 			return -1
 		}
 		return int64Value
 	} else {
-		logging.Default.Panicf("Environment variable %v must be set.", key)
+		log.Panicf("Environment variable %v must be set.", key)
 		return -1
 	}
 }
